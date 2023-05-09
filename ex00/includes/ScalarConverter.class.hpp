418e47e1906a45d46db681225a79991a157443fb
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-05-09 19:16:05 +0200

    .

---
+++ ex00/includes/ScalarConverter.class.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.class.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfuhlenb <vfuhlenb@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 19:11:59 by vfuhlenb          #+#    #+#             */
+/*   Updated: 2023/05/09 19:14:46 by vfuhlenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_H
+#define SCALAR_CONVERTER_H
+
+
+class ScalarConverter // static class (only static members)
+{
+	public:
+		
+};
+
+#endif
